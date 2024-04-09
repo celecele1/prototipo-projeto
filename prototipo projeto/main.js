807fe3c4416c2a3bc90b97b28d7f856e9ab24861
celecele1 <132454968+celecele1@users.noreply.github.com>
2024-04-09 12:09:25 -0300

    Add files via upload

--- prototipo projeto/main.js
+++ prototipo projeto/main.js
@@ -43,4 +43,49 @@ for ([inicialização]; [condição]; [incremento]) {
       numero *= 2;
       var numero = 10;
       numero /= 2; 
-                                           
+      const botoes = document.querySelectorAll(".botao");
+      const textos = document.querySelectorAll(".aba-conteudo");
+      botoes[i].classList.add("ativo");
+textos[i].classList.add("ativo");
+botoes[j].classList.remove("ativo");
+textos[j].classList.remove("ativo");
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    }
+}
+const contadores = document.querySelectorAll(".contador");
+contadores[0].textContent = "Contagem regressiva";
+const tempoObjetivo1 = new Date("2024-10-05");
+
+contadores[0].textContent = tempoObjetivo1;
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+let tempoAtual = new Date();
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+let tempoAtual = new Date();
+for (let i=-1 ; i<5; i++){
+    console.log(i);
+  }
+  const botao = document.querySelectorAll(".botao");
+  `const fundo= document.querySelector(".fundo-branco");`
+  for (let i=0;i<botao.length;i++){
+   botao[i].onclick=function(){
+      if(i==0){
+          fundo.classList.remove("fundo-branco");
+          fundo.classList.add("fundo-preto");
+      } else{
+        fundo.classList.remove("fundo-preto");
+       fundo.classList.add("fundo-branco");
+      }  }  }
+  
+
+                                    
